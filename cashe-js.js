7e6d54e1e6b325a23bbe84e783ca17a4f47a8696
ukengo <office@friedman.com.ua>
2022-07-04 15:19:06 +0300

    changes cashe

--- cashe-js.js
+++ cashe-js.js
@@ -9,25 +9,22 @@ function getDropDownArrayremarkCashe() {
 
 // отправка данных в таблицу
 function insertRowCache(data) {
-    console.log(data);
-        
-        const spredSheet = dataList1();
-        const key = 'PRHisAGc';
-        const colA = 'A';
-        const colB = 'B';
-        if (formatDateDDtireMMtireYYYY((preIndex(spredSheet, key, colA)[1][0])) != formatDateDDtireMMtireYYYY(new Date())) {
-            spredSheet.insertRows((preIndex(spredSheet, key, colB)[0]) + 1, 2);
-            spredSheet.getRange((preIndex(spredSheet, key, colB)[0]) + 2, 1, 1, 1).setValue(new Date());
-        }
-        spredSheet.insertRows((preIndex(spredSheet, key, colA)[0]) + 1);
-        spredSheet.getRange((preIndex(spredSheet, key, colA)[0]) + 1, 2, 1, 4).setValues(data);
-        const quantBlankRow = cellIndex(spredSheet, key) - preIndex(spredSheet, key, colB)[0] - 1
-        console.log(quantBlankRow);
-        if (quantBlankRow > 3) {
-            spredSheet.deleteRows(preIndex(spredSheet, key, colB)[0] + 1, quantBlankRow - 3);
-        }
+    const spredSheet = dataList1();
+    const key = 'PRHisAGc';
+    const colA = 'A';
+    const colB = 'B';
+    if (formatDateDDtireMMtireYYYY((preIndex(spredSheet, key, colA)[1][0])) != formatDateDDtireMMtireYYYY(new Date())) {
+        spredSheet.insertRows((preIndex(spredSheet, key, colB)[0]) + 1, 2);
+        spredSheet.getRange((preIndex(spredSheet, key, colB)[0]) + 2, 1, 1, 1).setValue(new Date());
     }
-    
+    spredSheet.insertRows((preIndex(spredSheet, key, colA)[0]) + 1);
+    spredSheet.getRange((preIndex(spredSheet, key, colA)[0]) + 1, 2, 1, 4).setValues(data);
+    const quantBlankRow = cellIndex(spredSheet, key) - preIndex(spredSheet, key, colB)[0] - 1
+    if (quantBlankRow > 3) {
+        spredSheet.deleteRows(preIndex(spredSheet, key, colB)[0] + 1, quantBlankRow - 3);
+    }
+}
+
 // получаем таблицу
 const cashe = () => {
     return 'https://docs.google.com/spreadsheets/d/1Y6ukb_iQ0JpPt2nTGvZx0HjWMJ2ACWD0iQDlV9Rjr7s/edit#gid=661962389'
@@ -54,11 +51,4 @@ function preIndex(spredSheet, key, col) {
 function cellIndex(spredSheet, key) {
     const dataArr = spredSheet.getRange('A1:A').getValues();
     return dataArr.flat().indexOf(key) + 1;
-}
-
-//форматирование даты в вид dd-mm-гггг 
-function formatDateDDtireMMtireYYYY(date) {
-    return ((date.getDate()) < 10 ? '0' : '') + (date.getDate()) + '-'
-        + ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1) + '-'
-        + date.getFullYear();
-}
+}}

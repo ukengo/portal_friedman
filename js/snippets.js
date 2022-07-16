f36e3a35cfa9cc7132392e943b64768f3573f56a
ukengo <office@friedman.com.ua>
2022-07-16 13:11:55 +0300

    added formatDateYYYYdotMMdotDD to snippets.js

--- js/snippets.js
+++ js/snippets.js
@@ -3,3 +3,7 @@
 function formatDateDDdotMMdotYYYY(date) {
     return Utilities.formatDate(new Date(date), 'Europe/Kiev', 'dd.MM.yyyy')
 }
+
+function formatDateYYYYdotMMdotDD(date) {
+    return Utilities.formatDate(new Date(date), 'Europe/Kiev', 'yyyy.MM.dd')
+}

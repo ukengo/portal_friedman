1455c4a89839af1f594aa45062743990482410ad
ukengo <office@friedman.com.ua>
2025-02-13 21:44:55 +0200

    rebase to main

--- js/cashe-rom.js
+++ js/cashe-rom.js
@@ -1,31 +1,36 @@
 // отримуємо всі дані з таблиці КЕШ Рома
-function getRecordsCashe() {
-  const key = 'PRHisAGc';
-  const spredSheet = dataCashe();
+function getRecordsCashe(key, spredSheet, rangeStart) {
   const lr = cellIndex(spredSheet, key);
-  const dataRecordsCashe = dataCashe().getRange(`A5692:E${lr - 1}`).getValues(); 
-  const dataMapCashe = dataRecordsCashe.map(x => [(x[0]), (x[1]), x[2], x[3], x[4]]);
-  const res = delSpaceCol(dataMapCashe);  
+  console.log(`A${rangeStart}:E${lr - 1}`);
+  const dataRecordsCashe = spredSheet.getRange(`A${rangeStart}:E${lr - 1}`).getValues();
+  return dataRecordsCashe
+}
 
-  /* const A3 = dataCashe().getRange(`A3`).getValue(); 
-  return JSON.stringify(A3);
- */
+// отримуємо всі дані з таблиці КЕШ Рома Лист1
+function getRecordsCashe1() {
+  const rangeStart = '5692'
+  const key = 'PRHisAGc'
+  const dataRecordsCashe = getRecordsCashe(key, dataCashe(), rangeStart)
+  const dataMapCashe = delSpaceCol(dataRecordsCashe).map((x, i) =>
+    [+rangeStart + i, formatDateDDdotMMdotYYYY(x[0]), x[1], x[2], x[3], x[4]]);
+  return JSON.stringify(dataMapCashe.filter(el => el[2] != ''));
+}
 
+//отримуємо всі дані з таблиці КЕШ Рома Аркуш1 (копия)
+function getRecordsCashe2() {
+  const rangeStart = '1'
+  const key = 'PRHisAGc'
+  const dataRecordsCashe = getRecordsCashe(key, dataCashe_2(), rangeStart)
+  //return JSON.stringify(dataRecordsCashe.filter(el => el[1] != ''));
+  return JSON.stringify(dataRecordsCashe);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  return JSON.stringify(res.reverse());
+function getRecordsCasheAll() {
+  
+  return {
+    casheRom1: getRecordsCashe1(),
+    casheRom2: getRecordsCashe2(),
+  }
 }
 
 //функція розмножує дані першого стовбця на пустоти знизу в першому стовбці і видаляє строки із пустим другим стовбцем
@@ -35,5 +40,29 @@ function delSpaceCol(data) {
       data[i + 1][0] = data[i][0]
     }
   }
-  return data.filter(el => el[1] != '')
+  //return data.filter(el => el[1] != '');
+  return data;
+}
+
+//запис строчки у таблицю КЕШ Рома
+function setRecordsCasheRom(dataModal) {
+  const data = [[
+    dataModal.casheInputArticle,
+    dataModal.casheInputWaste,
+    dataModal.casheInputArrival,
+    dataModal.casheInputNotes
+  ]];
+  console.log(data);
+  dataCashe().getRange(dataModal.numberRow, 2, 1, 4).setValues(data);
+}
+
+//insert
+function insertRowCasheRom(row) {
+  console.log(row)
+  const ss = dataCashe_2()
+  const A1Range = ss.getRange(`A1`)
+  const C1Range = ss.getRange(`C1`)
+  const A1Val = A1Range.getValue()
+  C1Range.setValue(A1Val)
+  A1Range.setValue(A1Val + row)
 }
--- js/load.js
+++ js/load.js
@@ -1,4 +1,4 @@
-
+/* 
 
 //===================================================//
 const URLREESTR = () => {
@@ -30,10 +30,6 @@ const dataSheet = () => {
   return ret
 }
 
-/* const dataSpiski = () => {
-    const ret = SpreadsheetApp.openByUrl(URLUPRAV()).getSheetByName("Списки")
-    return ret
-} */
 
 const dataFinance = () => {
   const ret = SpreadsheetApp.openByUrl(URLREESTR()).getSheetByName("Финансы")
@@ -116,3 +112,48 @@ function doGet() {
 }
 
 
+ */
+
+//===================================================//
+const URLS = {
+  REESTR: 'https://docs.google.com/spreadsheets/d/1ewk6ahWyREzSUe985l3zm_LblKpdjXs6rAHd71hXzwQ/edit#gid=627167327',
+  UPRAV: 'https://docs.google.com/spreadsheets/d/1xdJVfecdUCgtF_SHiC_IHbl8FP0Agl3slV_osyj6kxo/edit#gid=0',
+  BUH: 'https://docs.google.com/spreadsheets/d/1bd-YnMzDO8An3pGAniQXbFAPC1IWq5SSBlHK0pW2hVI/edit#gid=1139650230',
+  TERMINAL: 'https://docs.google.com/spreadsheets/d/1af9HbQ5cVnX_buV3rKjEvwuAVvSS-PuuDEHWrBqytcw/edit#gid=797242095',
+  KARANTIN: 'https://docs.google.com/spreadsheets/d/1OWkpbCN9oDoWtUm52ZdcNGTwiXp7qNgMxBYC10PmAsY/edit#gid=797242095',
+  CAHE: 'https://docs.google.com/spreadsheets/d/1Y6ukb_iQ0JpPt2nTGvZx0HjWMJ2ACWD0iQDlV9Rjr7s/edit?gid=928954653#gid=928954653'
+};
+
+const getSheet = (url, sheetName) => {
+  return SpreadsheetApp.openByUrl(url).getSheetByName(sheetName);
+};
+
+// Функции для получения нужных листов
+const dataInspektor = () => getSheet(URLS.REESTR, 'Инспектор');
+const dataSheet = () => getSheet(URLS.UPRAV, 'База');
+const dataFinance = () => getSheet(URLS.REESTR, 'Финансы');
+const dataSpiskiReestr = () => getSheet(URLS.REESTR, 'Списки');
+const dataVrabote = () => getSheet(URLS.REESTR, 'В работе');
+const dataArriwal = () => getSheet(URLS.REESTR, 'Прочие поступления');
+const dataWaste = () => getSheet(URLS.REESTR, 'Прочие траты');
+const dataBase = () => getSheet(URLS.REESTR, 'База');
+const dataTerminal = () => getSheet(URLS.TERMINAL, 'Оплата');
+const dataKarantin = () => getSheet(URLS.KARANTIN, 'Оплата');
+const dataJob = () => getSheet(URLS.REESTR, 'job');
+const ssBuhSheet = () => getSheet(URLS.BUH, 'РАХУНКИ');
+const dataUpravlIA = () => getSheet(URLS.REESTR, 'Управление ИА СЧЕТА');
+const dataCashe = () => getSheet(URLS.CAHE, 'Лист1');
+const dataCashe_2 = () => getSheet(URLS.CAHE, 'Аркуш1 (копия)');
+
+// Функции для работы с HTML
+function doGet() {
+  return HtmlService.createTemplateFromFile('main-HTML').evaluate()
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1 maximum-scale=1')
+    .setTitle('Portal Friedman');
+}
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .getContent();
+}

c375a28e4c17e4518d0affcfad47fa1d9ec7426f
ukengo <office@friedman.com.ua>
2025-06-01 12:39:18 +0300

    переносимо функції в клас

--- js/spisanieTermKar.js
+++ js/spisanieTermKar.js
@@ -1,40 +1,41 @@
 function insertTermKar(data) {
-
-  dataIns = [[data[0], data[5], '', data[1], true]]
-  if (data[4] === 'термінал') {
+  dataIns = [[data[0], data[4], '', data[1], true]]
+  if (data[3] === 'термінал') {
     dataTerminal().insertRowAfter(2);
     dataTerminal().getRange(3, 1, 1, 5).setValues(dataIns)
   }
-  if (data[4] === 'карантин') {
+  if (data[3] === 'карантин') {
 
     dataKarantin().insertRowAfter(2);
     dataKarantin().getRange(3, 1, 1, 5).setValues(dataIns)
   }
 }
 
-
 function vReestrTerminalKarantin(data) {
-  const sheetReestrTrata = dataWaste();
+  const sheetReestrTrata = dataWaste();  
   
   // получаем строчний массив счетов для поиска
-  let valueVhodString = [[data[0], data[5], '', data[1]]]
+  //let valueVhodString = [[data[0], data[5], '', data[1]]]
+  
   //  берем массив из таблицы Реестр оформлений, лист Прочие траты
-  let valueReestrTrata = sheetReestrTrata.getDataRange().getValues().slice(1);
+  //let valueReestrTrata = sheetReestrTrata.getDataRange().getValues().slice(1);
+  
   //  берем столбец с назначениями платежей
-  let valueReestrTrataNaznechenie = valueReestrTrata.map(x => x[3]);
+//  let valueReestrTrataNaznechenie = valueReestrTrata.map(x => x[3]);
+  
   //  берем столбец с назначениями платежей из таблицы Списання термінал
-  let valueVhodSchet = data[5];
+  //let valueVhodSchet = data[5];
 
-  if (data[4] === 'термінал') {
-    if (valueReestrTrataNaznechenie.toString().includes(valueVhodSchet) == false) {
-      let res = [Utilities.formatDate(new Date(data[0]), 'Europe/Kiev', 'dd.MM.yyyy'), data[1], 'тов', '', 'АВТОМОБІЛЬНІ ІНТЕЛЕКТУАЛЬНІ ТЕХНОЛОГІЇ ' + 'пп' + data[5]];
+  if (data[3] === 'термінал') {
+   // if (valueReestrTrataNaznechenie.toString().includes(valueVhodSchet) == false) {
+      let res = [Utilities.formatDate(new Date(data[0]), 'Europe/Kiev', 'dd.MM.yyyy'), data[1], 'тов', '', 'АВТОМОБІЛЬНІ ІНТЕЛЕКТУАЛЬНІ ТЕХНОЛОГІЇ ' + 'пп' + data[4]];
       sheetReestrTrata.appendRow(res);
-    }
+   // }
   }
-  if (data[4] === 'карантин') {
-    if (valueReestrTrataNaznechenie.toString().includes(valueVhodSchet) == false) {
-      let res = [Utilities.formatDate(new Date(data[0]), 'Europe/Kiev', 'dd.MM.yyyy'), data[1], 'тов', '', 'КАРАНТИН ' + '' + data[5]];
+  if (data[3] === 'карантин') {
+  //  if (valueReestrTrataNaznechenie.toString().includes(valueVhodSchet) == false) {
+      let res = [Utilities.formatDate(new Date(data[0]), 'Europe/Kiev', 'dd.MM.yyyy'), data[1], 'тов', '', 'КАРАНТИН ' + '' + data[4]];
       sheetReestrTrata.appendRow(res);
-    }
+  //  }
   }
 }

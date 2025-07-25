1d1e44a5cb9c55c6dc9567e383c23acc47cf125f
ukengo <office@friedman.com.ua>
2025-07-25 15:36:56 +0300

    replacing MailApp with GmailApp 	modified:   appsscript.json 	modified:   js/sendmailbuhinv.js

--- js/sendmailbuhinv.js
+++ js/sendmailbuhinv.js
@@ -1,35 +1,25 @@
-const sendEmailBuhInvoice = (sendbuhproekt, sendMail) => {
-  /* 
-    let { sendbuhcurrency, sendbuhprim, sendbuhforma, sendbuhvat, sendbuhproekt, sendbuhsumma, sendbuhcont } = sendMail; */
-  /*
-    let body = "";
-  
-    body += `<p style="font-size: 14px;">Проєкт: пр${sendbuhproekt}`;
-    if (!sendbuhsumma) {
-      body += `<br>${sendbuhcont}`;
-      // sendbuhproekt = '';
-    } else {
-      body += `<br>${sendbuhsumma} ${sendbuhcurrency} з ${sendbuhforma}`;
-      if (sendbuhforma.toLowerCase() != 'фоп') {
-        body += ` ${sendbuhvat}`;
-      }
-    }
-    if (sendbuhprim) {
-      body += `<br>${sendbuhprim}`;
-    }
-    if (sendbuhcont) {
-      body += `<br>${sendbuhcont}`;
-    }
-    body += `</p>`; */
-
-  /* MailApp.sendEmail("friedmanukraine@gmail.com", `Виставити рахунок до пр${sendbuhproekt}`, "тіло електронного листа", {
-    name: 'Віталий Мельник (Фрідман-Україна)',
-    htmlBody: sendMail
-  }); */
-  if (sendbuhproekt) {
+/* const sendEmailBuhInvoice = (sendbuhproekt, sendMail) => {
+    if (sendbuhproekt) {
     MailApp.sendEmail("friedmanukraine@gmail.com, vat.friedman@gmail.com", `Виставити рахунок до пр${sendbuhproekt}`, "тіло електронного листа", {
       name: 'Віталий Мельник (Фрідман-Україна)',
       htmlBody: sendMail
     })
+  }
+} */
+
+function sendEmailBuhInvoice(sendbuhproekt, sendMail) {
+  Logger.log('start')
+  try {
+    if (sendbuhproekt) {
+      GmailApp.sendEmail("friedmanukraine@gmail.com", `Виставити рахунок до пр${sendbuhproekt}`, "тіло електронного листа", {
+        name: 'Віталий Мельник (Фрідман-Україна)',
+        htmlBody: sendMail,
+       // cc: "vat.friedman@gmail.com"
+      });
+      Logger.log("Лист успішно надіслано через GmailApp");
+    }
+  } catch (e) {
+    Logger.log("Помилка: " + e.message + "\nStack: " + e.stack);
+    throw e;
   }
 }

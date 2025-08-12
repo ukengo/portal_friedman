fbeb7fb2a36018bc16011c6cfcef58570cc1f2af
ukengo <office@friedman.com.ua>
2025-08-12 15:58:04 +0300

    Виправлення авторизації пошти 	modified:   js/sendmailbuhinv.js 	modified:   reestr/tr-modal-script.html

--- js/sendmailbuhinv.js
+++ js/sendmailbuhinv.js
@@ -11,12 +11,12 @@ function sendEmailBuhInvoice(sendbuhproekt, sendMail) {
   Logger.log('start')
   try {
     if (sendbuhproekt) {
-      GmailApp.sendEmail("friedmanukraine@gmail.com", `Виставити рахунок до пр${sendbuhproekt}`, "тіло електронного листа", {
+      MailApp.sendEmail("friedmanukraine@gmail.com", `Виставити рахунок до пр${sendbuhproekt}`, "тіло електронного листа", {
         name: 'Віталий Мельник (Фрідман-Україна)',
         htmlBody: sendMail,
        // cc: "vat.friedman@gmail.com"
       });
-      Logger.log("Лист успішно надіслано через GmailApp");
+      Logger.log("Лист успішно надіслано через MailApp");
     }
   } catch (e) {
     Logger.log("Помилка: " + e.message + "\nStack: " + e.stack);
